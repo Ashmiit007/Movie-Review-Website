e3c357fee863290d7f8ec2c9491ad31703dedad9
Ashmiit007 <96116509+Ashmiit007@users.noreply.github.com>
2023-07-11 17:37:42 +0530

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -3,7 +3,61 @@ import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+  const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Scorcese');
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      // history.go(-1);
+      history.push('/');
+    })
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Review</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Movie title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Review :</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Review author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Scorcese">Scorcese</option>
+          <option value="Quentin">Quentin</option>
+        </select>
+        <button>Add Review</button>
+      </form>
+    </div>
+  );
+}
+ 
+export default Create;
   const [author, setAuthor] = useState('mario');
   const history = useHistory();
 
